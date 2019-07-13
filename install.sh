a97624447b5099bcffd4f36876ee05db8e1a3ff3
F8ASB <f8asb@hotmail.fr>
2019-07-13 17:11:57 +0200

    Add files via upload

--- install.sh
+++ install.sh
@@ -20,11 +20,11 @@ fi
 if [ $INSTALL = "SPOTNIK2HMI" ]; then
  
 echo "INSTALLATION DEPENDANCE PYTHON"
-#install gcc python-dev python-setuptools
+
 apt-get install python-dev
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-#python get-pip.py
 python3 get-pip.py
+pip3 install setuptools
 pip3 install requests
 pip3 install speedtest-cli
 pip3 install pyalsaaudio
