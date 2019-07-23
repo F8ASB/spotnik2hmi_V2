b3d022610c7be42f3872d0276975f9caf4b4ae8a
F8ASB <f8asb@hotmail.fr>
2019-07-23 21:49:12 +0200

    Update maj.sh

--- maj.sh
+++ maj.sh
@@ -6,11 +6,10 @@ mv /opt/spotnik/spotnik2hmi_V2/spotnik2hmi.py /opt/spotnik/spotnik2hmi_V2/old_ve
 mv /opt/spotnik/spotnik2hmi_V2/install.sh /opt/spotnik/spotnik2hmi_v2/old_version/install`date +%d%m%Y`.sh
 
 #Telechargement script
-wget https://raw.githubusercontent.com/F8ASB/spotnik2hmi_V2/master/fonctions.py -O /opt/spotnik/spotnik2hmi_V2/
-wget https://raw.githubusercontent.com/F8ASB/spotnik2hmi_V2/master/spotnik2hmi.py -O /opt/spotnik/spotnik2hmi_V2/
-wget https://raw.githubusercontent.com/F8ASB/spotnik2hmi_V2/master/install.sh -O /opt/spotnik/spotnik2hmi_V2/
-wget https://raw.githubusercontent.com/F8ASB/spotnik2hmi_V2/master/settings.py -O /opt/spotnik/spotnik2hmi_V2/
-
+wget /opt/spotnik/spotnik2hmi_V2/ https://raw.githubusercontent.com/F8ASB/spotnik2hmi_V2/master/fonctions.py
+wget /opt/spotnik/spotnik2hmi_V2/ https://raw.githubusercontent.com/F8ASB/spotnik2hmi_V2/master/spotnik2hmi.py 
+wget /opt/spotnik/spotnik2hmi_V2/https://raw.githubusercontent.com/F8ASB/spotnik2hmi_V2/master/install.sh 
+wget /opt/spotnik/spotnik2hmi_V2/https://raw.githubusercontent.com/F8ASB/spotnik2hmi_V2/master/settings.py 
 #Telechargement HMI
-wget https://github.com/F8ASB/spotnik2hmi_V2/blob/master/nextion/NX4832K035.tft -O /opt/spotnik/spotnik2hmi_V2/nextion/NX4832K035.tft
-wget https://github.com/F8ASB/spotnik2hmi_V2/blob/master/nextion/NX4832T035.tft -O /opt/spotnik/spotnik2hmi_V2/nextion/NX4832T035.tft
+wget /opt/spotnik/spotnik2hmi_V2/nextion/ https://github.com/F8ASB/spotnik2hmi_V2/blob/master/nextion/NX4832K035.tft 
+wget /opt/spotnik/spotnik2hmi_V2/nextion/ https://github.com/F8ASB/spotnik2hmi_V2/blob/master/nextion/NX4832T035.tft 
