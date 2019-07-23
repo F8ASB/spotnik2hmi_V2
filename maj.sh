1b5dc73f2a86299d5665d99ba0da7f7f45506d36
F8ASB <f8asb@hotmail.fr>
2019-07-23 21:49:39 +0200

    Update maj.sh

--- maj.sh
+++ maj.sh
@@ -8,8 +8,8 @@ mv /opt/spotnik/spotnik2hmi_V2/install.sh /opt/spotnik/spotnik2hmi_v2/old_versio
 #Telechargement script
 wget /opt/spotnik/spotnik2hmi_V2/ https://raw.githubusercontent.com/F8ASB/spotnik2hmi_V2/master/fonctions.py
 wget /opt/spotnik/spotnik2hmi_V2/ https://raw.githubusercontent.com/F8ASB/spotnik2hmi_V2/master/spotnik2hmi.py 
-wget /opt/spotnik/spotnik2hmi_V2/https://raw.githubusercontent.com/F8ASB/spotnik2hmi_V2/master/install.sh 
-wget /opt/spotnik/spotnik2hmi_V2/https://raw.githubusercontent.com/F8ASB/spotnik2hmi_V2/master/settings.py 
+wget /opt/spotnik/spotnik2hmi_V2/ https://raw.githubusercontent.com/F8ASB/spotnik2hmi_V2/master/install.sh 
+wget /opt/spotnik/spotnik2hmi_V2/ https://raw.githubusercontent.com/F8ASB/spotnik2hmi_V2/master/settings.py 
 #Telechargement HMI
 wget /opt/spotnik/spotnik2hmi_V2/nextion/ https://github.com/F8ASB/spotnik2hmi_V2/blob/master/nextion/NX4832K035.tft 
 wget /opt/spotnik/spotnik2hmi_V2/nextion/ https://github.com/F8ASB/spotnik2hmi_V2/blob/master/nextion/NX4832T035.tft 
